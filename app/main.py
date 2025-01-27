2fbe0276c37a7c426de65bc5ef5c591c61471f2d
Serhii-Cherh <chiper9@gmail.com>
2025-01-27 21:27:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_data):
+    for person_data in people_data:
+        name = person_data["name"]
+        age = person_data["age"]
+        Person(name, age)
+
+    for person_data in people_data:
+        name = person_data["name"]
+        person_instance = Person.people[name]
+
+        if "wife" in person_data and person_data["wife"] is not None:
+            wife_name = person_data["wife"]
+            person_instance.wife = Person.people[wife_name]
+
+        if "husband" in person_data and person_data["husband"] is not None:
+            husband_name = person_data["husband"]
+            person_instance.husband = Person.people[husband_name]
+
+    return [Person.people[person_data["name"]] for person_data in people_data]
